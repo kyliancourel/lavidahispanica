88c92fdf50999862027c6510c44d24af32cede6a
Kylian Courel <kyliancourel@MacBook-Pro-de-Kylian.local>
2025-01-23 02:14:48 +0100

    2ème réorganisation des fichiers et mise à jour des chemins pour GitHub Pages

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,6 +1,6 @@
 fetch('public/js/footer.js')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('footer-container').innerHTML = data;
-            })
-            .catch(error => console.error('Erreur de chargement de la navbar:', error));
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-container').innerHTML = data;
+    })
+    .catch(error => console.error('Erreur de chargement de la navbar:', error));
