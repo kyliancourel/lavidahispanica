b40b5d7880fde622eee1e199e3f19c53c9023d08
Kylian Courel <kyliancourel@MacBook-Pro-de-Kylian.local>
2025-01-23 12:18:44 +0100

    6ème réorganisation des fichiers et mise à jour des chemins pour GitHub Pages

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,4 +1,4 @@
-fetch('public/html/footer.html')
+fetch('../html/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-container').innerHTML = data;
