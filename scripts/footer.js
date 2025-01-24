64a4e6d2c596eca6c7cfa240e0865498700bd2e0
Kylian Courel <kyliancourel@MacBook-Pro-de-Kylian.local>
2025-01-24 03:42:10 +0100

    Mise à jour de toutes les pages html

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,8 @@
-fetch('../html/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('footer-container').innerHTML = data;
-    })
-    .catch(error => console.error('Erreur de chargement de la navbar:', error));
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("pages/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-container").innerHTML = data;
+        })
+        .catch(error => console.error("Erreur de chargement du footer:", error));
+});
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,8 @@
-fetch('../html/navbar.html')
-.then(response => response.text())
-.then(data => {
-    document.getElementById('navbar-container').innerHTML = data;
-})
-.catch(error => console.error('Erreur de chargement de la navbar:', error));
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("pages/navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+        })
+        .catch(error => console.error("Erreur de chargement de la navbar:", error));
+});
